cf1b511e45dd652e52b23f366eccabb32c73d5e7
Ryo Eguchi <eguchi@lifeistech.co.jp>
2016-10-31 22:45:45 +0900

    Great Commit

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    var number: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,47 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func plus () {
+        number = number + 1
+        label.text = String(number)
+        self.color()
+    
+    }
+    
+    @IBAction func minus() {
+        number = number - 1
+        label.text = String(number)
+        self.color()
+    }
+    
+    @IBAction func reset() {
+        number = 0
+        label.text = String(number)
+        self.color()
+    }
+    
+    @IBAction func times() {
+        number = number * 2
+        label.text = String(number)
+        self.color()
+    }
+    
+    @IBAction func waru() {
+        number = number / 2
+        label.text = String(number)
+        self.color()
+    }
+    
+    func color() {
+        
+        //numberが負の数になったら赤、0以上なら白に設定する
+        if number < 0 {
+            label.textColor = .red
+        }else{
+            label.textColor = .white
+        }
     }
 
 
